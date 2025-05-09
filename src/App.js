cb09be31324bf9440b207f0bbb45181b7bb1caa1
Hardik Sorathiya <sorathiyahardik000@gmail.com>
2025-05-09 15:36:40 +0530

    hero section start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import React from 'react';
+import { useEffect, React } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './component/Pages/Home/Home';
 import './assets/css/style.css';
 import './assets/css/media.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+    });
+  }, []);
   return (
     <BrowserRouter basename="/">
       <Routes>

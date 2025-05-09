ef4f705e252026d3a06b8e39de4b3650ba0c907c
Hardik Sorathiya <sorathiyahardik000@gmail.com>
2025-05-09 16:57:07 +0530

    Service section done

--- src/component/Pages/Home/Home.jsx
+++ src/component/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../../Include/Navbar/Navbar';
 import Hero from './Hero';
+import About from './About';
+import Services from './Services';
 
 export default function Home() {
   return (
     <>
    <Navbar />
    <Hero/>
+   <About/>
+   <Services/>
    </>
   )
 }
